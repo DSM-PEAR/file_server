2f250694585cf9c8ec1736ad5c98120136ae03be
Chaegeon <thscorjs1002@naver.com>
2020-10-26 19:05:22 +0900

    Test

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const app = express();
 
-const indexRouter = require('./routes/index');
-
 const port = 3000;
-
-
-app.use('/', indexRouter);
 
 app.listen(port, () => {
     console.log("server running on " + port);
