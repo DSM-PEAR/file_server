c98925eda41801688dfbad5bc74518dcea8d3eda
Chaegeon <thscorjs1002@naver.com>
2020-10-26 19:08:56 +0900

    test

--- test/index.js
+++ test/index.js
@@ -1,12 +1,5 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    describe('GET /', () => {
+describe('GET /', () => {
         it('should return 200 status code', () => {
             console.log('test');
         })
-    })
-})
-
-module.exports = router;
+});
