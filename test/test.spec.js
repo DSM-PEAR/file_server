4fcfaa09e45086dcad277595dd7eb07d7cc33fea
Chaegeon <thscorjs1002@naver.com>
2020-11-05 15:14:46 +0900

    File system

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,6 +1,7 @@
 
 module.exports = {
-    test: () => { describe('GET /', () => {
+    test: () => { 
+        describe('GET /', () => {
             it('should return 200 status code', () => {
                 console.log('test');
             })
