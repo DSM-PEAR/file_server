3048d500710262f1d3d30bafb6ee623778d2e7ae
sonchaegeon <thscorjs1002@naver.com>
2021-02-17 15:41:50 +0900

    [REFACTOR] promise syntax get file inform controller

--- controller/notice-controller.js
+++ controller/notice-controller.js
@@ -3,107 +3,118 @@ const config = require('../config/multer');
 const multer = require('multer');
 
 exports.deleteFile = async (req, res) => {
-    db.notice_tbl.findOne({
-        where: {
-            id: req.params.file_id
-        }
+  db.notice_tbl
+    .findOne({
+      where: {
+        id: req.params.file_id,
+      },
     })
-    .then(result => {
-        if(result === null) res.status(404).send("파일이 존재하지 않습니다");
-        var file = process.cwd() + "/uploads/noticeFiles/" + result.path;
-        config.fs.unlinkSync(file);
+    .then((result) => {
+      if (result === null) res.status(404).send('파일이 존재하지 않습니다');
+      var file = process.cwd() + '/uploads/noticeFiles/' + result.path;
+      config.fs.unlinkSync(file);
 
-        db.notice_tbl.destroy({
-            where: {
-                id: req.params.file_id
-            }
-        })
-        res.json(success);
+      db.notice_tbl.destroy({
+        where: {
+          id: req.params.file_id,
+        },
+      });
+      res.json(success);
     })
-    .catch(err => res.json(err));
-}
+    .catch((err) => res.json(err));
+};
 
 exports.downloadNoticeFile = async (req, res) => {
-    db.notice_tbl.findOne({
-        where: {
-            id: req.params.file_id
-        }
-    }).then(result => {
-        var file = process.cwd() + "/uploads/noticeFiles/" + result.path;
-        if(config.fs.existsSync(file)){
-            var filename = config.path.basename(file);
-            var mimetype = config.mime.getType(file);
-
-            res.setHeader('Content-disposition', 'attachment; filename=' + filename);
-            res.setHeader('Content-type', mimetype);
-
-            var filestream = config.fs.createReadStream(file);
-            filestream.pipe(res);
-        } else {
-            res.status(500).send('해당 파일이 없습니다.');
-            return;
-        }
-    }).catch(error => {
-        console.log(error);
-        res.status(404).send('파일을 찾을 수 없습니다');
-        return;
+  db.notice_tbl
+    .findOne({
+      where: {
+        id: req.params.file_id,
+      },
     })
-}
+    .then((result) => {
+      var file = process.cwd() + '/uploads/noticeFiles/' + result.path;
+      if (config.fs.existsSync(file)) {
+        var filename = config.path.basename(file);
+        var mimetype = config.mime.getType(file);
+
+        res.setHeader(
+          'Content-disposition',
+          'attachment; filename=' + filename
+        );
+        res.setHeader('Content-type', mimetype);
+
+        var filestream = config.fs.createReadStream(file);
+        filestream.pipe(res);
+      } else {
+        res.status(500).send('해당 파일이 없습니다.');
+        return;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).send('파일을 찾을 수 없습니다');
+      return;
+    });
+};
 
 exports.getNoticeFile = async (req, res) => {
-    if(!Number.isInteger(parseInt(req.params.notice_id))) {
-        res.status(400).send("notice_id 다시 확인해주세요.")
-        return 400;
-    }
+  if (!Number.isInteger(parseInt(req.params.notice_id))) {
+    return res.status(400).send('notice_id 다시 확인해주세요.');
+  }
 
-    db.notice_tbl.findAll({
-        attributes: ['id', 'path'],
-        where: {
-            notice_id: req.params.notice_id,
-        }
-    })
-    .then(result => {
-        if(result[0] === undefined) {
-            res.status(404).send("File not found");
-        } else {
-            res.json(result);
-        }
-    })
-    .catch(err => res.json(err));
-}
+  const noticeFiles = await db.notice_tbl.findAll({
+    attributes: ['id', 'path'],
+    where: {
+      notice_id: req.params.notice_id,
+    },
+  });
+
+  if (noticeFiles[0] === undefined) {
+    res.status(404).send('File not found');
+  } else {
+    res.json(noticeFiles);
+  }
+};
 
 exports.uploadNoticeFile = async (req, res) => {
-    config.upload.single('noticeFile')(req, res, (err) => {
-        if(err instanceof multer.MulterError){
-            res.status(400).send("form name 다시 확인해주세요");
-        } else if(err){
-            console.log(err);
-            res.status(500).send("문의주세요");
-        }
-        db.notice_tbl.create({
-        path : req.file.filename,
-        notice_id : req.params.notice_id
-        }).then(result => res.json(result))
-        .catch(err => res.json(err));
+  config.upload.single('noticeFile')(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      res.status(400).send('form name 다시 확인해주세요');
+    } else if (err) {
+      console.log(err);
+      res.status(500).send('문의주세요');
     }
-)}
+    db.notice_tbl
+      .create({
+        path: req.file.filename,
+        notice_id: req.params.notice_id,
+      })
+      .then((result) => res.json(result))
+      .catch((err) => res.json(err));
+  });
+};
 
 exports.modifyNoticeFile = async (req, res) => {
-    config.upload.single('noticeFile')(req, res, (err) => {
-        if(err instanceof multer.MulterError){
-            res.status(400).send("form name 다시 확인해주세요");
-        } else if(err){
-            consolee.log(err);
-            res.status(500).send("문의주세요");
+  config.upload.single('noticeFile')(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      res.status(400).send('form name 다시 확인해주세요');
+    } else if (err) {
+      consolee.log(err);
+      res.status(500).send('문의주세요');
+    }
+    console.log(req.params.file_id);
+    db.notice_tbl
+      .update(
+        {
+          path: req.file.filename,
+        },
+        {
+          where: {
+            id: req.params.file_id,
+          },
         }
-        console.log(req.params.file_id);
-        db.notice_tbl.update({
-            path: req.file.filename,
-        }, {
-            where: {
-                id: req.params.file_id,
-            }
-        }).then(() => res.send("PUT SUCCESS"))
-        .catch(err => res.json(err));
-    })
-}
+      )
+      .then(() => res.send('PUT SUCCESS'))
+      .catch((err) => res.json(err));
+  });
+};
--- controller/report-controller.js
+++ controller/report-controller.js
@@ -3,107 +3,122 @@ const config = require('../config/multer');
 const multer = require('multer');
 
 exports.deleteFile = async (req, res) => {
-    db.report_tbl.findOne({
-        where: {
-            id: req.params.file_id
-        }
+  db.report_tbl
+    .findOne({
+      where: {
+        id: req.params.file_id,
+      },
     })
-    .then(result => {
-        if(result === null) res.status(404).send("파일이 존재하지 않습니다");
-        var file = process.cwd() + "/uploads/reportFiles/" + result.path;
-        config.fs.unlinkSync(file);
+    .then((result) => {
+      if (result === null) res.status(404).send('파일이 존재하지 않습니다');
+      var file = process.cwd() + '/uploads/reportFiles/' + result.path;
+      config.fs.unlinkSync(file);
 
-        db.report_tbl.destroy({
-            where: {
-                id: req.params.file_id
-            }
-        })
-        res.json(success);
+      db.report_tbl.destroy({
+        where: {
+          id: req.params.file_id,
+        },
+      });
+      res.json(success);
     })
-    .catch(err => res.json(err));
-}
+    .catch((err) => res.json(err));
+};
 
 exports.downloadReportFile = async (req, res) => {
-    db.report_tbl.findOne({
-        where: {
-            id: req.params.file_id
-        }
-    }).then(result => {
-        var file = process.cwd() + "/uploads/reportFiles/" + result.path;
-        if(config.fs.existsSync(file)){
-            var filename = config.path.basename(file);
-            var mimetype = config.mime.getType(file);
-
-            res.setHeader('Content-disposition', 'attachment; filename=' + config.iconv.decode(config.iconv.encode(filename, "UTF-8"), 'ISO-8859-1'));
-            res.setHeader('Content-type', mimetype);
-
-            var filestream = config.fs.createReadStream(file);
-            filestream.pipe(res);
-        } else {
-            res.status(500).send('해당 파일이 없습니다.');
-            return;
-        }
-    }).catch(error => {
-        console.log(error);
-        res.status(404).send('파일을 찾을 수 없습니다');
-        return;
+  db.report_tbl
+    .findOne({
+      where: {
+        id: req.params.file_id,
+      },
     })
-}
+    .then((result) => {
+      var file = process.cwd() + '/uploads/reportFiles/' + result.path;
+      if (config.fs.existsSync(file)) {
+        var filename = config.path.basename(file);
+        var mimetype = config.mime.getType(file);
+
+        res.setHeader(
+          'Content-disposition',
+          'attachment; filename=' +
+            config.iconv.decode(
+              config.iconv.encode(filename, 'UTF-8'),
+              'ISO-8859-1'
+            )
+        );
+        res.setHeader('Content-type', mimetype);
+
+        var filestream = config.fs.createReadStream(file);
+        filestream.pipe(res);
+      } else {
+        res.status(500).send('해당 파일이 없습니다.');
+        return;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).send('파일을 찾을 수 없습니다');
+      return;
+    });
+};
 
 exports.getReportFile = async (req, res) => {
-    if(!Number.isInteger(parseInt(req.params.report_id))) {
-        res.status(400).send("report_id 다시 확인해주세요.")
-        return 400;
-    }
+  if (!Number.isInteger(parseInt(req.params.report_id))) {
+    return res.status(400).send('report_id 다시 확인해주세요.');
+  }
 
-    db.report_tbl.findAll({
-        attributes: ['id', 'path'],
-        where: {
-            report_id: req.params.report_id,
-        }
-    })
-    .then(result => {
-        if(result[0] === undefined) {
-            res.status(404).send("File not found");
-        } else {
-            res.json(result);
-        }
-    })
-    .catch(err => res.json(err));
-}
+  const reportFiles = await db.report_tbl.findAll({
+    attributes: ['id', 'path'],
+    where: {
+      report_id: req.params.report_id,
+    },
+  });
+
+  if (reportFiles[0] === undefined) {
+    res.status(404).send('File not found');
+  } else {
+    res.json(reportFiles);
+  }
+};
 
 exports.uploadReportFile = async (req, res) => {
-    config.upload.single('reportFile')(req, res, (err) => {
-        if(err instanceof multer.MulterError){
-            res.status(400).send("form name 다시 확인해주세요");
-        } else if(err){
-            console.log(err);
-            res.status(500).send("문의주세요");
-        }
-        db.report_tbl.create({
-        path : req.file.filename,
-        report_id : req.params.report_id
-        }).then(result => res.json(result))
-        .catch(err => res.json(err));
+  config.upload.single('reportFile')(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      res.status(400).send('form name 다시 확인해주세요');
+    } else if (err) {
+      console.log(err);
+      res.status(500).send('문의주세요');
     }
-)}
+    db.report_tbl
+      .create({
+        path: req.file.filename,
+        report_id: req.params.report_id,
+      })
+      .then((result) => res.json(result))
+      .catch((err) => res.json(err));
+  });
+};
 
 exports.modifyReportFile = async (req, res) => {
-    config.upload.single('reportFile')(req, res, (err) => {
-        if(err instanceof multer.MulterError){
-            res.status(400).send("form name 다시 확인해주세요");
-        } else if(err){
-            consolee.log(err);
-            res.status(500).send("문의주세요");
+  config.upload.single('reportFile')(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      res.status(400).send('form name 다시 확인해주세요');
+    } else if (err) {
+      consolee.log(err);
+      res.status(500).send('문의주세요');
+    }
+    console.log(req.params.file_id);
+    db.report_tbl
+      .update(
+        {
+          path: req.file.filename,
+        },
+        {
+          where: {
+            id: req.params.file_id,
+          },
         }
-        console.log(req.params.file_id);
-        db.report_tbl.update({
-            path: req.file.filename,
-        }, {
-            where: {
-                id: req.params.file_id,
-            }
-        }).then(() => res.send("PUT SUCCESS"))
-        .catch(err => res.json(err));
-    })
-}
+      )
+      .then(() => res.send('PUT SUCCESS'))
+      .catch((err) => res.json(err));
+  });
+};
