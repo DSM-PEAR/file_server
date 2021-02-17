8ab082372578c56d25dd0d9f82e338224401fe22
sonchaegeon <thscorjs1002@naver.com>
2021-02-17 15:39:58 +0900

    [REFACTOR] models

--- models/index.js
+++ models/index.js
@@ -3,7 +3,12 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development'; // production
 const config = require('../config/config')[env];
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
 const db = {};
 
--- models/report.js
+++ models/report.js
@@ -1,16 +1,19 @@
 module.exports = (sequelize, DataTypes) => {
-    const report_tbl = sequelize.define('report_tbl', {
-        path: {
-            type: DataTypes.STRING(40),
-            allowNull: false
-        },
-        report_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        }
-
-    }, {
-        freezeTableName: true
-    });
-    return report_tbl;
+  const report_tbl = sequelize.define(
+    'report_tbl',
+    {
+      path: {
+        type: DataTypes.STRING(40),
+        allowNull: false,
+      },
+      report_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      freezeTableName: true,
+    }
+  );
+  return report_tbl;
 };
