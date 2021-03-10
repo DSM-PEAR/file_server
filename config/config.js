97fddea68ffb713d9f14262f69c29b53f4597740
sonchaegeon <thscorjs1002@naver.com>
2021-03-10 10:40:46 +0900

    [FIX] config
    deploy setting

--- config/config.js
+++ config/config.js
@@ -2,27 +2,27 @@ require('dotenv').config();
 const env = process.env;
 
 const development = {
-  "username": env.DB_USERNAME,
-  "password": env.DB_PASSWORD,
-  "database": env.DB_NAME,
-  "host": env.DB_HOST,
-  "dialect": "mysql"
+  username: env.DB_USERNAME,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME,
+  host: env.DB_HOST,
+  dialect: 'mysql',
 };
 
 const production = {
-  "username": env.DB_USERNAME,
-  "password": env.DB_PASSWORD,
-  "database": env.DB_NAME,
-  "host": env.DB_HOST,
-  "dialect": "mysql"
+  username: env.DB_USERNAME,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME,
+  host: env.DB_HOST,
+  dialect: 'mysql',
 };
 
 const test = {
-  "username": env.DB_USERNAME,
-  "password": env.DB_PASSWORD,
-  "database": env.DB_NAME,
-  "host": env.DB_HOST,
-  "dialect": "mysql"
+  username: env.DB_USERNAME,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME,
+  host: env.DB_HOST,
+  dialect: 'mysql',
 };
 
 module.exports = { development, production, test };
