35de767a109c6ffeefb5b0b762b8df9bfdde6804
sonchaegeon <thscorjs1002@naver.com>
2021-05-17 13:16:23 +0900

    [UPDATE] token validation all report router

--- routes/reportRouter.js
+++ routes/reportRouter.js
@@ -5,12 +5,12 @@ const { TokenValidation } = require('../middlewares/verifyToken');
 
 router.put('/:file_id', TokenValidation, FileController.modifyReportFile);
 router.delete('/:file_id', TokenValidation, FileController.deleteFile);
-router.get('/:file_id', FileController.downloadReportFile);
+router.get('/:file_id', TokenValidation, FileController.downloadReportFile);
 
-router.get('/files/:report_id', FileController.getReportFile);
+router.get('/files/:report_id', TokenValidation, FileController.getReportFile);
 router.post(
   '/files/:report_id',
   TokenValidation,
-  FileController.uploadReportFile
+  FileController.uploadReportFile,
 );
 module.exports = router;
